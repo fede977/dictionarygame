89a1f5b0215a8029864e6c54e97f5ecda2285d39
fede977 <rullofederico16@gmail.com>
2017-10-20 13:35:39 +0100

    Answer checking and redirection to win/lose pages

--- app/controllers/word_controller.rb
+++ app/controllers/word_controller.rb
@@ -1,5 +1,22 @@
 class WordController < ApplicationController
   def index
+    @Answer = [] 
+    params.each do |k,v|
+      if k.start_with? "answer"
+        @Answer.push(v)
+      end
+    end
+    
+    @word_array=JSON.parse(session[:word_array])
+    puts "#{@Answer} != #{@word_array}"
+    if !@Answer.empty?
+      if @Answer != @word_array
+        redirect_to '/word/lose'
+      else
+        redirect_to '/word/win'
+      end
+    end
+    
     @Display = Hash.new
     (1..4).each do |w|
       w = Wordnik.word.get_random_word["word"]
@@ -7,20 +24,13 @@ class WordController < ApplicationController
       @Display[w] = Wordnik.word.get_definitions(w)[0]["text"]
     end
     @word_array = @Display.keys
+    
+    session[:word_array]=@word_array.to_s
+
     @word_array = @word_array.shuffle
 
     @definition_array = @Display.values
     @definition_array = @definition_array
-
-    @Answer = Hash.new 
-    (1...4).each do |a|
-      a = params[:answer]
-      @Answer[a]
-    end
-    
-    if @Answer == @Display
-      redirect_to '/GameOver'
-    end
   end
 
   def win
