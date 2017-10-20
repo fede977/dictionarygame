2bba79b29c132c41e85447469b1d99cfeddbd2ab
fede977 <rullofederico16@gmail.com>
2017-10-20 18:41:00 +0100

    db redo

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 20171019211915) do
-
+  create_table "new_games", force: :cascade do |t|
+    t.string "name"
+    t.integer "score"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+  
   create_table "welcomes", force: :cascade do |t|
     t.string "name"
     t.integer "score"
